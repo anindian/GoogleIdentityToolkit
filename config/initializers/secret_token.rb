2620cc5414db30f0486fd69cff36de0b8384bf1c
Azri <azri@azri-System-Product-Name.(none)>
2013-01-22 13:49:49 +0530

    Sample Application using Google Identity Toolkit V2

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Toolkitnew::Application.config.secret_token = 'd245f27dede8120d90181f4e8e57891bc15971ffd5fd0a72ec767ae5caf56c5b168c2ab6a5cca9ce3753da3678d682465245165b0214c9daa05e8d8530d7ec49'
